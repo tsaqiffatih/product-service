3656a10525e4fa40533921aeebe43c1c008581e7
tsaqiffatih <tsaqiffatih92@gmail.com>
2024-10-12 21:07:42 +0700

    update ProductHandler

--- handlers/productHandler.go
+++ handlers/productHandler.go
@@ -21,9 +21,29 @@ func init() {
 // Get All Products
 func GetProducts(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+		offset, _ := strconv.Atoi(r.URL.Query().Get("offset"))
+		search := r.URL.Query().Get("search")
+
+		if limit == 0 {
+			limit = 10 // Default limit
+		}
+
 		var products []models.Product
-		db.Find(&products)
-		json.NewEncoder(w).Encode(products)
+		query := db.Limit(limit).Offset(offset)
+
+		if search != "" {
+			query = query.Where("name LIKE ?", "%"+search+"%")
+		}
+
+		query.Find(&products)
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"status":  "success",
+			"message": "Get Products successfully",
+			"data":    products,
+		})
 	}
 }
 
@@ -35,10 +55,23 @@ func GetProduct(db *gorm.DB) http.HandlerFunc {
 
 		var product models.Product
 		if err := db.First(&product, id).Error; err != nil {
-			http.Error(w, "Product not found", http.StatusNotFound)
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "fail",
+				"error": map[string]interface{}{
+					"code":    http.StatusNotFound,
+					"message": "Product not found",
+				},
+			})
 			return
 		}
-		json.NewEncoder(w).Encode(product)
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"status":  "success",
+			"message": "Get Product successfully",
+			"data":    product,
+		})
 	}
 }
 
@@ -46,9 +79,52 @@ func GetProduct(db *gorm.DB) http.HandlerFunc {
 func CreateProduct(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var product models.Product
-		json.NewDecoder(r.Body).Decode(&product)
-		db.Create(&product)
-		json.NewEncoder(w).Encode(product)
+
+		err := json.NewDecoder(r.Body).Decode(&product)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "fail",
+				"error": map[string]interface{}{
+					"code":    http.StatusBadRequest,
+					"message": "Invalid request payload",
+				},
+			})
+			return
+		}
+
+		// validation input
+		err = validate.Struct(product)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "fail",
+				"error": map[string]interface{}{
+					"code":    http.StatusBadRequest,
+					"message": err.Error(),
+				},
+			})
+			return
+		}
+
+		if err := db.Create(&product).Error; err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "fail",
+				"error": map[string]interface{}{
+					"code":    http.StatusInternalServerError,
+					"message": "Failed to create product",
+				},
+			})
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"status":  "success",
+			"message": "Create Product successfully",
+			"data":    product,
+		})
 	}
 }
 
@@ -60,13 +136,26 @@ func UpdateProduct(db *gorm.DB) http.HandlerFunc {
 
 		var product models.Product
 		if err := db.First(&product, id).Error; err != nil {
-			http.Error(w, "Product not found", http.StatusNotFound)
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "fail",
+				"error": map[string]interface{}{
+					"code":    http.StatusNotFound,
+					"message": "Product not found",
+				},
+			})
 			return
 		}
 
 		json.NewDecoder(r.Body).Decode(&product)
 		db.Save(&product)
-		json.NewEncoder(w).Encode(product)
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"status":  "success",
+			"message": "Update Product successfully",
+			"data":    product,
+		})
 	}
 }
 
@@ -78,11 +167,36 @@ func DeleteProduct(db *gorm.DB) http.HandlerFunc {
 
 		var product models.Product
 		if err := db.First(&product, id).Error; err != nil {
-			http.Error(w, "Product not found", http.StatusNotFound)
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "fail",
+				"error": map[string]interface{}{
+					"code":    http.StatusNotFound,
+					"message": "Product not found",
+				},
+			})
 			return
 		}
 
-		db.Delete(&product)
+		// soft delete
+		if err := db.Delete(&product).Error; err != nil {
+			// http.Error(w, "Failed to delete product", http.StatusInternalServerError)
+			// return
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"status": "fail",
+				"error": map[string]interface{}{
+					"code":    http.StatusInternalServerError,
+					"message": "Failed to delete product",
+				},
+			})
+			return
+		}
+
 		w.WriteHeader(http.StatusNoContent)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"status":  "success",
+			"message": "Product deleted successfully",
+		})
 	}
 }
